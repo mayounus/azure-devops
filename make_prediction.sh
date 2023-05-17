49a4522eb6a3581b03502dd808392725b91007ae
Mohammad Younus <mayounus@gmail.com>
2023-05-17 14:46:50 +0000

    Updated make_prediciton.sh

--- make_prediction.sh
+++ make_prediction.sh
@@ -25,4 +25,4 @@ curl -d '{
    }
 }'\
      -H "Content-Type: application/json" \
-     -X POST http://azDevOpsMLUdacity.azurewebsites.net:$PORT/predict
+     -X POST http://localhost:$PORT/predict
