7a034839b945ebbed36890843f8f0527b4fd6cad
Mohammad Younus <mayounus@gmail.com>
2023-05-22 14:06:05 +0000

    update make_predict_azure_app.sh

--- make_predict_azure_app.sh
+++ make_predict_azure_app.sh
@@ -25,5 +25,5 @@ curl -d '{
    }
 }'\
      -H "Content-Type: application/json" \
-     -X POST https://azDevOpsMLUdacity.azurewebsites.net:$PORT/predict 
+     -X POST https://azdevopsmludacity.azurewebsites.net:$PORT/predict 
      #your application name <yourappname>goes here
